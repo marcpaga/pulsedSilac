c52d04946c86de2d5610efb784ed992a7e5076cb
marcpaga <marc.pages@live.com>
2019-04-30 15:05:49 +0200

    internal to merge metaoptions

--- R/internals.R
+++ R/internals.R
@@ -48,53 +48,15 @@ setMethod('hasRowData', 'ProteomicsExperiment', function(x){
 })
 
 
-###### Metaoptions =============================================================
-setGeneric('metaoptions', function(x){
-  standardGeneric('metaoptions')
-})
 
-setMethod('metaoptions', 'ProteinExperiment', function(x){
-
-  metaoptions_names <- c('conditionCol',
-                         'timeCol',
-                         'replicateIntCol',
-                         'replicateTimeCol')
-
-  return(metadata(x)[metaoptions_names])
-
-})
-
-setMethod('metaoptions', 'PeptideExperiment', function(x){
-
-  metaoptions_names <- c('conditionCol',
-                         'timeCol',
-                         'replicateIntCol',
-                         'replicateTimeCol')
-
-  return(metadata(x)[metaoptions_names])
-
-})
-
-setMethod('metaoptions', 'ProteomicsExperiment', function(x){
-
-  metaoptions_names <- c('conditionCol',
-                         'timeCol',
-                         'replicateIntCol',
-                         'replicateTimeCol',
-                         'idColProt',
-                         'idColPep',
-                         'linkedSubset',
-                         'subsetMode')
-
-  return(metadata(x)[metaoptions_names])
-
-})
 
 ###### Specific metaoptions ====================================================
 
+## retrieve metaoptions =====
+
 hasMetaoption <- function(x, option) {
 
-  op <- metadata(x)[[option]]
+  op <- metaoptions(x)[[option]]
   if (is.na(op)) {
     txt <- sprintf('Not defined in metadata: %s.', option)
     stop(txt)
@@ -107,7 +69,7 @@ hasMetaoption <- function(x, option) {
 metaoptionInColData <- function(x, option) {
 
   if (hasMetaoption(x, option)) {
-    op <- metadata(x)[[option]]
+    op <- metaoptions(x)[[option]]
 
     if (op %in% names(colData(x))) {
       return(TRUE)
@@ -122,8 +84,65 @@ metaoptionInColData <- function(x, option) {
 giveMetaoption <- function(x, option) {
 
   if (metaoptionInColData(x, option)) {
-    op <- metadata(x)[[option]]
+    op <- metaoptions(x)[[option]]
     return(op)
   }
 
+}
+
+## merge metaoptions lists ====
+
+.mergeMetaoptions <- function(x, y) {
+
+  finalLength <- max(length(x), length(y))
+  finalNames <- unique(c(names(x), names(y)))
+  new_metaoptions <- list()
+
+  for (i in seq_len(finalLength)) {
+
+    new_metaoptions[[i]] <- .compareOptions(x[[finalNames[i]]],
+                                            y[[finalNames[i]]],
+                                            finalNames[i])
+
+  }
+
+  names(new_metaoptions) <- finalNames
+  return(new_metaoptions)
+}
+
+.compareOptions <- function(x, y, option) {
+
+  ## one of the elements is not in the other list
+  if (is.null(x)) {
+    return(y)
+  }
+  if (is.null(y)) {
+    return(x)
+  }
+
+  ## not defined in any of the lists
+  if (is.na(x) & is.na(y)) {
+    return(NA)
+  }
+
+  ## defined in one but not in the other
+  if (is.na(x) & !is.na(y)) {
+    return(y)
+  }
+  if (!is.na(x) & is.na(y)) {
+    return(x)
+  }
+
+  ## defined in both
+  if (x == y) {
+    return(x)
+  } else if (x != y) {
+    txt <- sprintf(
+      paste('The %s metaoption was defined differently: %s and %s in the two',
+            'experiments. The first one was used.'),
+      option, x, y
+    )
+    warning(txt)
+    return(x)
+  }
 }
