1078a015d312858c245a375c36f16a8172394630
marcpaga <marc.pages@live.com>
2019-05-01 16:22:58 +0200

    added methods for colData setter for Protein and Peptide because the SummarizedExperiment method has DataFrame signature

--- R/coldata.R
+++ R/coldata.R
@@ -1,4 +1,29 @@
 #' @export
+#' @importFrom S4Vectors DataFrame
+setMethod('colData<-', 'ProteinExperiment', function(x, ..., value) {
+
+  if (is(value, 'data.frame')) {
+    value <- DataFrame(value)
+  }
+  x@colData <- value
+  validObject(x)
+  return(x)
+
+})
+
+#' @export
+setMethod('colData<-', 'PeptideExperiment', function(x, ..., value) {
+
+  if (is(value, 'data.frame')) {
+    value <- DataFrame(value)
+  }
+  x@colData <- value
+  validObject(x)
+  return(x)
+
+})
+
+#' @export
 setMethod('colData', 'ProteomicsExperiment', function(x, ...) {
 
   return(x@colData)
@@ -8,7 +33,13 @@ setMethod('colData', 'ProteomicsExperiment', function(x, ...) {
 #' @export
 setMethod('colData<-', 'ProteomicsExperiment', function(x, ..., value) {
 
+  if (is(value, 'data.frame')) {
+    value <- DataFrame(value)
+  }
+
   x@colData <- value
+  colData(x@ProteinExperiment) <- value
+  colData(x@PeptideExperiment) <- value
   validObject(x)
   return(x)
 
