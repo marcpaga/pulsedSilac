cb167f207a5d15f575e0745ee31e59abe52037be
marcpaga <marc.pages@live.com>
2019-05-01 15:08:45 +0200

    forgot to export colData

--- R/coldata.R
+++ R/coldata.R
@@ -1,9 +1,11 @@
+#' @export
 setMethod('colData', 'ProteomicsExperiment', function(x, ...) {
 
   return(x@colData)
 
 })
 
+#' @export
 setMethod('colData<-', 'ProteomicsExperiment', function(x, ..., value) {
 
   x@colData <- value
