d6df95ddf3a33d19eee3c8fdfef8d723b12fffd8
marcpaga <marc.pages@live.com>
2019-05-02 13:30:05 +0200

    added rbind methods tests

---
+++ tests/testthat/test-rbind.R
@@ -0,0 +1,44 @@
+context("test-rbind")
+
+test_that("rbind works", {
+
+  testProtExp <- testList[[1]]
+  testPeptExp <- testList[[2]]
+  testPE <- testList[[3]]
+
+  ## ProteinExperiment
+  testPE <- testList[[1]]
+
+  expect_silent(testPE2 <- rbind(testPE[1:2,], testPE[3,]))
+  expect_equivalent(testPE, testPE2)
+  expect_equivalent(colData(testPE), colData(testPE2))
+  expect_equivalent(rowData(testPE), rowData(testPE2))
+  expect_equivalent(metadata(testPE), metadata(testPE2))
+  expect_equivalent(metaoptions(testPE), metaoptions(testPE2))
+
+  ## PeptideExperiment
+  testPE <- testList[[2]]
+
+  expect_silent(testPE2 <- rbind(testPE[1:2,], testPE[3:5,]))
+  expect_equivalent(testPE, testPE2)
+  expect_equivalent(colData(testPE), colData(testPE2))
+  expect_equivalent(rowData(testPE), rowData(testPE2))
+  expect_equivalent(metadata(testPE), metadata(testPE2))
+  expect_equivalent(metaoptions(testPE), metaoptions(testPE2))
+
+  ## ProteomicsExperiment
+  testPE <- testList[[3]]
+
+  expect_silent(testPE2 <- rbind(testPE[1:2,], testPE[3,]))
+  expect_equivalent(testPE@ProteinExperiment, testPE2@ProteinExperiment)
+  expect_equivalent(testPE@PeptideExperiment, testPE2@PeptideExperiment)
+  expect_equivalent(colData(testPE), colData(testPE2))
+  expect_equivalent(rowDataProt(testPE), rowDataProt(testPE2))
+  expect_equivalent(rowDataPept(testPE), rowDataPept(testPE2))
+  expect_equivalent(metadata(testPE), metadata(testPE2))
+  expect_equivalent(metaoptions(testPE), metaoptions(testPE2))
+  expect_equivalent(linkerDf(testPE), linkerDf(testPE2))
+
+
+
+})
