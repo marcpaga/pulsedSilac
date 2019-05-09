8cc2aeb35d02c8b8e0a9f65c8b22f022770f647b
marcpaga <marc.pages@live.com>
2019-05-09 10:09:46 +0200

    palette was missing for ProteomicsExperiment

--- R/barplotFeaturesCounts.R
+++ R/barplotFeaturesCounts.R
@@ -209,6 +209,10 @@ setMethod('barplotFeaturesCounts',
     return(plotDf)
   }
 
+  ## cb palette
+  cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
+                 "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
   if (all(is.na(plotDf$conditionCol))) {
     ggplot(data = plotDf,
            aes(x = sample,
--- R/barplottimecoverage.R
+++ R/barplottimecoverage.R
@@ -295,6 +295,10 @@ setMethod('barplotTimeCoverage',
     return(plotDf)
   }
 
+  ## cb palette
+  cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
+                 "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
   if (all(is.na(plotDf$group))) {
     ggplot(data = plotDf,
            aes(x = counts,
