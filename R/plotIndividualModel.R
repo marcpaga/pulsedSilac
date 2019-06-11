277cbb192ccd9bdeb04764535e3e4a351fc352af
marcpaga <marc.pages@live.com>
2019-06-11 14:53:22 +0200

    added generics and methods to plot individual proteins and peptide models

--- R/plotIndividualModel.R
+++ R/plotIndividualModel.R
@@ -1,23 +1,225 @@
 #' @export
-plotIndividualModel <- function(modelList, num) {
+setGeneric('plotIndividualModel', function(x, ...){
+  standardGeneric('plotIndividualModel')
+})
 
+
+#' @title Fitted model(s) for a feature
+#'
+#' @description Plot the model fit for a specific protein/peptide in different
+#' conditions.
+#'
+#' @param x A \code{ProteinExperiment}, \code{PeptideExperiment} or
+#' \code{ProteomicsExperiment} object.
+#' @param modelList A list containing all the model objects, the output of
+#' \link{\code{modelTurnover}} with returnModel as TRUE.
+#' @param num The feature number to be plotted.
+#' @param return A \code{character} indicating whether to return a plot ('plot')
+#' or a \code{data.frame} ('data.frame'); (default = 'plot').
+#' @return A scatter plot with a fitted line or a \code{data.frame}.
+#' @export
+#' @import ggplot2
+setMethod('plotIndividualModel',
+          'ProteinExperiment',
+          function(x,
+                   modelList,
+                   num,
+                   return = 'plot') {
+
+  ## cb palette
+  cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
+                 "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+  ## models related to that feature
   miniList <- lapply(modelList, "[[", num)
 
+  ## extract the metaoptions from the attributes of the list
   time <- attributes(modelList)[['time']]
   cond <- attributes(modelList)[['cond']]
   loopCols <- attributes(modelList)[['loopCols']]
-  mod$m$lhs()
-  mod$m$fitted()
 
-  plotDf <- data.frame(originalval = mod$m$lhs(),
-                       predictedval = mod$m$fitted(),
-                       time = time[1:7],
-                       condition = cond[1:7])
+  mat <- assaysProt(x)[[attributes(modelList)[['assayName']]]]
+  vals <- mat[num, ]
 
-  ggplot(data = plotDf) +
-    geom_line(aes(x = time, y = predictedval)) +
-    geom_point(aes(x = time, y = originalval), color = 'red') +
-    theme_classic()
+  ## extract the data for each condition and join them in a plotable dataframe
+  ## we have two dataframes, one for the original values, and another for the
+  ## fitted model. The second one is longer to have a smoother curve.
+  for (i in seq_along(loopCols)) {
+    if (i == 1) {
+      dfOriginalList <- list()
+      dfFittedList <- list()
+    }
+    mod <- miniList[[i]]
+    if (!is(mod, 'nls')) {
+      warning(paste('No model found for', unique(cond[loopCols[[i]]])))
+      next
+    }
+    origDf <- data.frame(originalval = vals[loopCols[[i]]],
+                         time = time[loopCols[[i]]],
+                         condition = cond[loopCols[[i]]])
+    dfOriginalList[[i]] <- origDf
+
+    all_times <- seq(from = min(time[loopCols[[i]]]),
+                     to = max(time[loopCols[[i]]]),
+                     by = 1)
+    fittedDf <- data.frame(time = all_times,
+                           condition = rep(unique(cond[loopCols[[i]]]),
+                                           times = length(all_times)),
+                           fittedval = predict(mod, list(t = all_times)))
+    dfFittedList[[i]] <- fittedDf
+
+  }
+
+  origPlotDf <- do.call('rbind', dfOriginalList)
+  fittedPlotDf <- do.call('rbind', dfFittedList)
+
+  if (is.null(origPlotDf)) {
+    stop('No models found for any condition')
+  }
+
+  ## if the data frames are wanted then they are returned in a list
+  if (return == 'data.frame') {
+    return(list(original_data = origPlotDf, fitted_data = fittedPlotDf))
+  }
+
+  ## the plot itself
+  ggplot() +
+    geom_line(data = fittedPlotDf, aes(x = time,
+                                       y = fittedval,
+                                       group = condition,
+                                       color = condition)) +
+    geom_point(data = origPlotDf, aes(x = time,
+                                      y = originalval,
+                                      color = condition)) +
+    scale_color_manual(values = cbPalette) +
+    theme_classic() +
+    ylab(label = attributes(modelList)[['assayName']])
+
+})
 
 
-}
+setMethod('plotIndividualModel',
+          'PeptideExperiment',
+          function(x,
+                   modelList,
+                   num,
+                   return = 'plot') {
+
+  ## cb palette
+  cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
+                 "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+  ## models related to that feature
+  miniList <- lapply(modelList, "[[", num)
+
+  ## extract the metaoptions from the attributes of the list
+  time <- attributes(modelList)[['time']]
+  cond <- attributes(modelList)[['cond']]
+  loopCols <- attributes(modelList)[['loopCols']]
+
+  ## get the rows of the data
+  if (attributes(modelList)[['mode']] == 'peptide') {
+
+    mat <- assays(x[num,])[[attributes(modelList)[['assayName']]]]
+    repeats <- 1
+  } else if (attributes(modelList)[['mode']] == 'grouped') {
+
+    protId <- unique(attributes(modelList)[['prot']])[num]
+    rows <- which(attributes(modelList)[['prot']] == protId)
+    mat <- assays(x)[[attributes(modelList)[['assayName']]]][rows, , drop = FALSE]
+    repeats <- nrow(mat)
+  }
+
+  ## extract the data for each condition and join them in a plotable dataframe
+  ## we have two dataframes, one for the original values, and another for the
+  ## fitted model. The second one is longer to have a smoother curve.
+  for (i in seq_along(loopCols)) {
+    if (i == 1) {
+      dfOriginalList <- list()
+      dfFittedList <- list()
+    }
+    mod <- miniList[[i]]
+    if (!is(mod, 'nls')) {
+      warning(paste('No model found for', unique(cond[loopCols[[i]]])))
+      next
+    }
+
+    origDf <- data.frame(originalval = as.vector(mat[,loopCols[[i]]]),
+                         time = rep(time[loopCols[[i]]], each = repeats),
+                         condition = rep(cond[loopCols[[i]]], each = repeats))
+
+    dfOriginalList[[i]] <- origDf
+
+    all_times <- seq(from = min(time[loopCols[[i]]]),
+                     to = max(time[loopCols[[i]]]),
+                     by = 1)
+
+    fittedDf <- data.frame(time = all_times,
+                           condition = rep(unique(cond[loopCols[[i]]]),
+                                           times = length(all_times)),
+                           fittedval = predict(mod, list(t = all_times)))
+
+    dfFittedList[[i]] <- fittedDf
+
+  }
+
+  origPlotDf <- do.call('rbind', dfOriginalList)
+  fittedPlotDf <- do.call('rbind', dfFittedList)
+
+  if (is.null(origPlotDf)) {
+    stop('No models found for any condition')
+  }
+
+  ## if the data frames are wanted then they are returned in a list
+  if (return == 'data.frame') {
+    return(list(original_data = origPlotDf, fitted_data = fittedPlotDf))
+  }
+
+  ## the plot itself
+  ggplot() +
+    geom_line(data = fittedPlotDf, aes(x = time,
+                                       y = fittedval,
+                                       group = condition,
+                                       color = condition)) +
+    geom_point(data = origPlotDf, aes(x = time,
+                                      y = originalval,
+                                      color = condition)) +
+    scale_color_manual(values = cbPalette) +
+    theme_classic() +
+    ylab(label = attributes(modelList)[['assayName']])
+
+})
+
+
+
+setMethod('plotIndividualModel',
+          'ProteomicsExperiment',
+          function(x,
+                   modelList,
+                   num,
+                   return = 'plot') {
+
+  if (attributes(modelList)[['mode']] == 'protein') {
+
+    plotIndividualModel(x = x@ProteinExperiment,
+                        modelList = modelList,
+                        num = num,
+                        return = return)
+
+  } else if (attributes(modelList)[['mode']] == 'peptide') {
+
+    plotIndividualModel(x = x@PeptideExperiment,
+                        modelList = modelList,
+                        num = num,
+                        return = return)
+
+  } else if (attributes(modelList)[['mode']] == 'grouped') {
+
+    plotIndividualModel(x = x@PeptideExperiment,
+                        modelList = modelList,
+                        num = num,
+                        return = return)
+
+  }
+
+})
