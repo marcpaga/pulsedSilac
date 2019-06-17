e10a2e03fef3d7c7d343540a6363d9a563276700
marcpaga <marc.pages@live.com>
2019-06-17 11:36:30 +0200

    added compare aic function

---
+++ R/compareaic.R
@@ -0,0 +1,55 @@
+#' Calculates the probability of each model from a set of models.
+#'
+#' For a given set of AIC from models, the probability of each model relative
+#' to the rest of the models of the set is calculated using the following
+#' formula:
+#' \deqn{\prod AIC_{i} = \frac{exp(\frac{AIC_{min}-AIC_{i}}{2})}{\sum_{j}exp(\frac{AIC_{min}-AIC_{j}}{2})}}
+#'
+#' @param ... a \code{list} with the model metrics,
+#' the output from \code{\link{modelTurnover}} and \code{\link{calculateAIC}}.
+#' @return a \code{list} with a matrix for each experiment condition. The matrix
+#' contains the probabilities of each model (columns) for each protein/peptide
+#' (rows).
+#'
+#' @seealso \code{\link{calculateAIC},
+#'                \link{modelTurnover}}
+#' @export
+compareAIC <- function(...) {
+
+  inputLists <- list(...)
+  n_models <- length(inputLists)
+
+  aicsList <- lapply(inputLists, '[[', 'AIC')
+
+  n_conditions <- length(attributes(inputLists[[1]])$loopCols)
+  outputList <- list()
+
+  for (i in seq_len(n_conditions)) {
+    for (j in seq_len(n_models)) {
+      if (j == 1) {
+        tempList <- list()
+      }
+      tempList[[j]] <- aicsList[[j]][,i]
+    }
+    aicMatrix <- do.call('cbind', tempList)
+    probs <- t(apply(aicMatrix, 1, .compareAIC))
+    outputList[[i]] <- probs
+  }
+
+  return(outputList)
+}
+
+#' Calculates the probability of a set of AIC values
+#'
+#' @param aics Numeric vector containing the AIC values
+#' @return A numeric vector of the same length as the input with the probability
+#' of each model relative to each other.
+#' @keywords internal
+.compareAIC <- function(aics) {
+
+  probs <- vapply(aics, FUN.VALUE = c(1), FUN = function(x){
+    (exp((min(aics) - x)/2))/(sum(exp((min(aics)- aics)/2)))
+  })
+  return(probs)
+
+}
