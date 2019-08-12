497f90b556ee8415ea2eee9c8bc52c81c28599cf
marcpaga <marc.pages@live.com>
2019-08-12 11:33:56 +0200

    added tests for compareAIC

--- tests/testthat/test-aic.R
+++ tests/testthat/test-aic.R
@@ -48,4 +48,53 @@ test_that("calculateAIC works", {
 
 test_that("compareAIC works", {
 
+  wormsPE <- calculateIsotopeFraction(wormsPE, ratioAssay = 'ratio')
+  testPE <- wormsPE[1:10,]
+  testPE <- ProtExp(testPE)
+
+
+  expect_silent(ml1 <- modelTurnover(x = testPE,
+                                     assayName = 'fraction',
+                                     formula = 'fraction ~ 1-exp(-k*t)',
+                                     start = list(k = 0.02),
+                                     robust = FALSE,
+                                     verbose = FALSE,
+                                     returnModel = TRUE))
+  expect_silent(ml1 <- calculateAIC(ml1))
+
+  expect_error(compareAIC(ml1))
+
+  expect_silent(ml2 <- modelTurnover(x = testPE,
+                                     assayName = 'fraction',
+                                     formula = 'fraction ~ 1-exp(-k*t) + b',
+                                     start = list(k = 0.02, b = 0),
+                                     robust = FALSE,
+                                     verbose = FALSE,
+                                     returnModel = TRUE))
+
+  expect_error(compareAIC(ml1, ml2))
+  expect_silent(ml2 <- calculateAIC(ml2))
+
+  expect_silent(ml_compare <- compareAIC(ml1, ml2))
+  expect_is(ml_compare, 'list')
+  expect_length(ml_compare, 1)
+  expect_length(unlist(ml_compare, FALSE), 2)
+
+  expect_silent(ml3 <- modelTurnover(x = testPE,
+                                     assayName = 'fraction',
+                                     formula = 'fraction ~ 1.2-exp(-k*t) + b',
+                                     start = list(k = 0.02, b = 0),
+                                     robust = FALSE,
+                                     verbose = FALSE,
+                                     returnModel = TRUE))
+  expect_silent(ml3 <- calculateAIC(ml3))
+
+  expect_silent(ml_compare <- compareAIC(ml1, ml2, ml3))
+  expect_is(ml_compare, 'list')
+  expect_length(ml_compare, 1)
+  expect_length(unlist(ml_compare, FALSE), 3)
+
+
+
+
 })
